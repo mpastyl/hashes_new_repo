9f57f9ef6cba5706db5f608a7f1e1c0138f68acf
Mpampis Stylianopoulos (jimsiak diplom) <mpastyl@scirouter>
2014-10-06 11:45:56 +0300

    Some more graphs

---
+++ scripts/plot-init_size-VS_throughput.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+import sys
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import numpy as np
+
+nthreads_axis = []
+all_results = []
+
+for filename in sys.argv[1:]:
+
+	fp = open(filename, "r")
+
+	local_nthreads_axis = []
+	local_results = []
+
+	line = fp.readline()
+	while line:
+		if "init_hash_size:" in line:
+			nthreads = int(line.split()[1])
+			local_nthreads_axis.append(nthreads)
+		elif "Ops/usec:" in line:
+			throughput = float(line.split()[1])
+			local_results.append(throughput)
+
+		line = fp.readline()
+
+	fp.close()
+
+	if len(nthreads_axis) == 0:
+		nthreads_axis = list(local_nthreads_axis)
+	elif len(nthreads_axis) != len(local_nthreads_axis):
+		print "File", filename, "contains less nthreads values."
+		sys.exit(1)
+	elif nthreads_axis != local_nthreads_axis:
+		print "File", filename, "contains different nthreads values."
+		sys.exit(1)
+
+	all_results.append(local_results)
+
+print nthreads_axis
+print all_results
+
+ax = plt.subplot("111")
+
+for res in all_results:
+	ax.plot(np.arange(len(nthreads_axis)), res)
+
+#plt.title()
+plt.grid(True)
+plt.xticks(np.arange(len(nthreads_axis)), nthreads_axis)
+ax.set_xlabel("#init_hash_size")
+ax.set_ylabel("throuhput (ops/usec)")
+plt.savefig("nthreads-VS-throughput.pdf")
--- scripts/test_script.sh
+++ scripts/test_script.sh
@@ -16,12 +16,21 @@ cd ../
 
 #preinsetion  size
 ##for z in 512 1024 2048 4096 ;
-for z in 32768 131072 ;
+##for z in 32768 131072 ;
+##do 
+##    for i in 1 2 3 6 12 24; 
+##    do
+##        for name in "global_lock" "striped" "refinable" "split_ordered"; 
+##            do ./main.$name -t$i -s 4096 -z $z -l80 -i10 >> out_${name}_init_size4096_preinsertion_$z.out;
+##        done;
+##    done;
+##done;
+
+
+#corss init_size
+for s in 2048 4096 8192 16384 32768 ;
 do 
-    for i in 1 2 3 6 12 24; 
-    do
         for name in "global_lock" "striped" "refinable" "split_ordered"; 
-            do ./main.$name -t$i -s 4096 -z $z -l80 -i10 >> out_${name}_init_size4096_preinsertion_$z.out;
+            do ./main.$name -t24 -s $s -l80 -i10 >> out_${name}_cross_init_size.out;
         done;
-    done;
 done;
