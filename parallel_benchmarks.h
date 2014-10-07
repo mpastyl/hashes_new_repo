d87926161cc46343b47bdedbbba0124ed8b048d8
Mpampis Stylianopoulos (jimsiak diplom) <mpastyl@scirouter>
2014-10-07 13:05:15 +0300

    Found HUGE problem with false, sharing. Need to make new graphs

--- parallel_benchmarks.h
+++ parallel_benchmarks.h
@@ -9,9 +9,11 @@
 #endif
 
 typedef struct {
-	unsigned tid;
-
+	
+    unsigned tid;
+    char pad0[(64-sizeof(unsigned))/sizeof(char)];
 	unsigned nr_operations;
+    char pad1[(64-sizeof(unsigned))/sizeof(char)];
 
 	unsigned operations, 
 	         lookups, 
@@ -19,22 +21,30 @@ typedef struct {
 	         deletions,
 	         real_insertions,
 	         real_deletions;
+    char pad2[(64-6*sizeof(unsigned))/sizeof(char)];
     long long int value_sum;        
+    char pad3[(64-sizeof(long long int))/sizeof(char)];
 #ifdef WORKLOAD_TIME
 	unsigned int time_to_leave;
+    char pad4[(64-sizeof(unsigned int))/sizeof(char)];
 #endif
 
 #ifdef SPLIT_ORDERED
     unsigned long long *prev;
+    char pad5[(64-sizeof(unsigned long long *))/sizeof(char)];
     unsigned long long curr;
+    char pad6[(64-sizeof(unsigned long long ))/sizeof(char)];
     unsigned long long next;
+    char pad7[(64-sizeof(unsigned long long))/sizeof(char)];
 #endif
 
 #ifdef TSX
 	txstats_t txstats;
+    char pad8[(64-sizeof(txstats_t))/sizeof(char)];
 #endif
 
 	tsc_t insert_lock_set_tsc;
+    char pad9[(64-sizeof(tsc_t))/sizeof(char)];
 
 //	tsc_t operations_tsc;
 } params_t;
--- scripts/test_script.sh
+++ scripts/test_script.sh
@@ -36,12 +36,22 @@ cd ../
 #done;
 
 #big table
-for i in 1 2 3 6 12 24;
-do
-        ##for name in "global_lock" "striped" "refinable" "split_ordered" "non_blocking"; 
-        ##    do ./main.$name -t$i -s 16777216 -z 1048576 -l80 -i10 >> out_bit_table_${name}.out;
-        ##done;
-        for name in "cuckoo"; 
-            do ./main.$name -t$i -s 16777216 -z 1048576 -l80 -i10 -k 2 >> out_bit_table_${name}_h.out;
-        done;
+#for i in 1 2 3 6 12 24;
+#do
+#        ##for name in "global_lock" "striped" "refinable" "split_ordered" "non_blocking"; 
+#        ##    do ./main.$name -t$i -s 16777216 -z 1048576 -l80 -i10 >> out_bit_table_${name}.out;
+#        ##done;
+#        for name in "cuckoo"; 
+#            do ./main.$name -t$i -s 16777216 -z 1048576 -l80 -i10 -k 2 >> out_bit_table_${name}_h.out;
+#        done;
+#done;
+
+
+#corss init_size
+for i in 1 2 3 4 5 6 7 8 ;
+do 
+                    ./main.tsx -t$i -s 4096  -l98 -i2 >> out_tsx_4096_98_2_0.out;
+                    ./main.tsx -t$i -s 4096  -l97 -i3 >> out_tsx_4096_97_3_0.out;
+                    ./main.tsx -t$i -s 4096  -l96 -i4 >> out_tsx_4096_96_4_0.out;
+                    ./main.tsx -t$i -s 4096  -l95 -i5 >> out_tsx_4096_95_5_0.out;
 done;
